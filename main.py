73aff384f46312303df45502f66418f67620b310
ramesh <rameshkumarvrs@gmail.com>
2026-07-26 14:37:11 +0530

    added the get employeef rom the lsit logic using for

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+emp = [
+    {"name": 'ramesh', "id": 101, 'place': "Namakkal"},
+     {"name": 'Haran', "id": 102, 'place': "Newzeland"},
+      {"name": 'Riya', "id": 103, 'place': "karur"}
+]
+
 
 @app.get("/display")
 def view():
@@ -10,3 +16,10 @@ def view():
 @app.get("/display/{id}")
 def display_id(id: int):
     return {"message": id}
+
+@app.get("/employee/{id}")
+def get_emp(id:int):
+    for e in emp:
+        if e["id"] == id:
+            return e
+
