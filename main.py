120090d012d2c7bc89eb4ce963866d4b451d6d09
ramesh <rameshkumarvrs@gmail.com>
2026-08-03 14:49:22 +0530

    added the bootstrap ui for the fast api templates

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI, Form, File, UploadFile, HTTPException, Response,Cookie
 from fastapi import Request
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app =FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 templates = Jinja2Templates(directory="templates")
+
 
 # class manf(BaseModel):
 #     name : str
