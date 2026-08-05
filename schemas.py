9342f2448021b54059dd6b44ad21aeab983330b4
ramesh <rameshkumarvrs@gmail.com>
2026-08-05 18:07:47 +0530

    Added the schemas.rb file along woth the post base model

---
+++ schemas.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel, ConfigDict, Field
+
+
+class PostBase(BaseModel):
+    title: str = Field(min_length=1, max_length=100)
+    content: str = Field(min_length=1)
+    author: str = Field(min_length=1, max_length=50)
+    
