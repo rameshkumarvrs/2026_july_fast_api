d33de54cde8ab14fd8983d90f2df54894f025bcf
ramesh <rameshkumarvrs@gmail.com>
2026-08-11 19:27:40 +0530

    added the individual post make as clickable

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException,status
 from fastapi.staticfiles import StaticFiles
 
 from fastapi.templating import Jinja2Templates
@@ -31,6 +31,16 @@ def home(request: Request):
     return templates.TemplateResponse(request, "home.html", {"posts": posts, "title": "Home"},)
 
 
+@app.get("/posts/{post_id}", include_in_schema=False)
+def post_page(post_id: int, request: Request):
+    for post in posts:
+        if post.get("id") == post_id:
+            title = post['title'][:50]
+            return templates.TemplateResponse(request, "post.html", {"post": post, "title": title})
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="post not found")   
+        
+
+
 @app.get("/api/posts")
 def get_posts():
     return posts
@@ -44,5 +54,5 @@ def get_post(post_id: int):
         if post.get("id") == post_id:
             return post
 
-    return {"error": "post not found"}   
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No post available")
 
