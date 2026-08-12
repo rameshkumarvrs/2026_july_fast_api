e1f7683b175bce461ee02694d5c3beec1f7cc8b2
ramesh <rameshkumarvrs@gmail.com>
2026-08-12 21:39:15 +0530

    added the user base and usercreate and user response model in the schema.rb

--- database.py
+++ database.py
@@ -1,20 +1,23 @@
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import DeclarativeBase, sessionmaker
+## database.py
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-# SQLALCHAMY_DATABAS_URL = "sqlite:///./blog.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./blog.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False},
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
-# engine = create_engine(
-#     SQLALCHAMY_DATABAS_URL,
-#     connect_args={"check_same_thread": False},
-# )
+class Base(DeclarativeBase):
+    pass
 
 
-# sessionlocal = sessionmaker(autocommit= False, autoflush=False, bind=engine)
+def get_db():
+    with SessionLocal() as db:
+        yield db
 
-# class Base(DeclarativeBase):
-#     pass
 
-# def get_db():
-#     with sessionlocal() as db:
-#         yield db
