b82184467ac80ceb00b15848e2314af1d0ff1657
ramesh <rameshkumarvrs@gmail.com>
2026-08-12 14:59:09 +0530

    added thepydantic validation for the fields to submit the p;ost form

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 from starlette.exceptions import HTTPException as StarletteHTTPException
 from schemas import PostCreate, PostResponse
+from typing import List
 
 from fastapi.templating import Jinja2Templates
 
@@ -45,14 +46,53 @@ def post_page(post_id: int, request: Request):
         
 
 
-@app.get("/api/posts")
+@app.get("/api/posts", response_model=List[PostResponse])
 def get_posts():
     return posts
 
 
+# ## Create Post
+# @app.post(
+#     "/api/posts",
+#     response_model=PostResponse,
+#     status_code=status.HTTP_201_CREATED,
+# )
+# def create_post(post: PostCreate):
+#     new_id = max(p["id"] for p in posts) + 1 if posts else 1
+#     new_post = {
+#         "id": new_id,
+#         "author": post.author,
+#         "title": post.title,
+#         "content": post.content,
+#         "date_posted": "April 23, 2025",
+#     }
+#     posts.append(new_post)
+#     return new_post
+
+
+
+
+
+
+## Create Post code by ramesh
+
+@app.post("/api/posts", response_model=PostResponse, status_code=status.HTTP_201_CREATED)
+def create_post(post: PostCreate):
+    new_id = max(p["id"] for p in posts) +1 if posts else 1
+    new_post ={
+        "id": new_id,
+        "author": post.author,
+        "title": post.title,
+        "content": post.content,
+        "date_posted": "April 25, 2025"
+    }
+    posts.append(new_post)
+    return new_post
+
+
 #This code for get the single post
 
-@app.get("/api/posts/{post_id}")
+@app.get("/api/posts/{post_id}", response_model=PostResponse)
 def get_post(post_id: int):
     for post in posts:
         if post.get("id") == post_id:
